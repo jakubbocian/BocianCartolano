4fd4c5dfae1759c27231b163185008228d62c664
jakubbocian <jakub.bocian@mpolo.it>
2022-09-29 08:38:14 +0200

    Inizio descrizione protocollo e agg. UML

--- es20Client/src/es20client/Es20Client.java
+++ es20Client/src/es20client/Es20Client.java
@@ -1,18 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package es20client;
 
+import java.io.*;
+import java.net.*;
+import java.nio.*;
 /**
  *
  * @author JAKUBBOCIAN
  */
 public class Es20Client {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
