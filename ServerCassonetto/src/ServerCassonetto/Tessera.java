fd74ecc78a2054f6296c590731b4c3da50059948
ZTDavide <dadocarto503@gmail.com>
2022-10-04 09:37:43 +0200

    Server crea tessera

---
+++ ServerCassonetto/src/ServerCassonetto/Tessera.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ServerCassonetto;
+
+/**
+ *
+ * @author JAKUBBOCIAN
+ */
+public class Tessera {
+    private int id;
+    private boolean valida;
+
+    public Tessera(int id) {
+        this.id = id;
+        this.valida = true;
+    }
+
+    public boolean isValida() {
+        return valida;
+    }
+
+    public void setValida(boolean valida) {
+        this.valida = valida;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
