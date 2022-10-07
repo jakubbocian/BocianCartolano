b9222464709b836c00157a83971cc42ca8e208df
jakubbocian <jakub.bocian@mpolo.it>
2022-10-07 09:52:07 +0200

    Server funzionante

--- ServerCassonetto/src/ServerCassonetto/ServerCassonetto.java
+++ ServerCassonetto/src/ServerCassonetto/ServerCassonetto.java
@@ -1,111 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ServerCassonetto;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
-import java.nio.ByteBuffer;
+import java.io.*;
+import java.net.*;
+import java.nio.*;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author JAKUBBOCIAN
- */
-public class ServerCassonetto extends Thread {
+public class ServerCassonetto extends Thread{
+    private int numTessera = 0;
+    private ArrayList tessere = new ArrayList();
 
-    /**
-     * @param args the command line arguments
-     */
-    ArrayList<Tessera> tessereReg = new ArrayList();
-    DatagramSocket socket;
+    
+    
+    private int generaId(){
+        return numTessera++;
+    }
 
+    private DatagramSocket socket;
+    
+    // costruttore (richiede il numero di porta del servizio)
     public ServerCassonetto(int port) throws SocketException {
-
         socket = new DatagramSocket(port);
-        socket.setSoTimeout(2000);
-
+        socket.setSoTimeout(1000); // 1000ms = 1s
     }
 
     public void run() {
-        
-        DatagramPacket answer, request;
         byte[] buffer = new byte[1];
         ByteBuffer data;
-        int reqtype;
+        DatagramPacket answer, request;
+        
+        int idTessera;
         
         while (!Thread.interrupted()) {
             try {
                 request = new DatagramPacket(buffer, buffer.length);
-                
+                // attesa ricezione datagram di richiesta (tempo massimo di attesa: 1s)
                 socket.receive(request);
-                data = ByteBuffer.wrap(buffer, 0, 4);
-                
-                reqtype = data.getInt();
-                System.out.println("Richiesta n: " + reqtype);
-                
-                /*answer = new DatagramPacket( data.array(), 8, request.getAddress(),
-                request.getPort()); server.socket.send(answer);
-                server.socket.send(answer);*/
-            } catch (IOException ex) {
-                System.out.println("Errore IOException!");
+                // incapsulazione del buffer della richiesta in un byte-buffer della dimensione di 4 valori float
+                data = ByteBuffer.wrap(buffer, 0, 1);
+                System.out.println("Richiesta n." + data.get());
+                /*
+                // incapsulazione del buffer della risposta in un byte-buffer della dimensione di 1 valore double
+                data = ByteBuffer.wrap(buffer, 0, 8);
+                // inserimento del valore double nel byte-buffer
+                // costruzione del datagram da trasmettere a partire dal contenuto del byte-buffer
+                answer = new DatagramPacket( data.array(), 8, request.getAddress(),
+                request.getPort()); socket.send(answer);
+                socket.send(answer);*/
+            }
+            catch (SocketTimeoutException exception) {
+            }
+            catch (IOException exception) {
             }
         }
-        socket.close();
-
+        socket.close(); // chiusura del socket
     }
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        
         int c;
 
         try {
-
             ServerCassonetto server = new ServerCassonetto(12345);
             server.start();
             c = System.in.read();
             server.interrupt();
             server.join();
-
-        } catch (IOException exception) {
-            System.err.println("Errore IOException!");
-        } catch (InterruptedException exception) {
-            System.err.println("Errore InterruptedException!");
         }
-
-    }
-
-    public int creaTessera() {
-
-        Tessera t = new Tessera(this.tessereReg.size() + 1);
-        tessereReg.add(t);
-        return t.getId();
-
-    }
-
-    public boolean checkTessera(int id) {
-
-        for (Tessera t : tessereReg) {
-
-            if (t.getId() == id) {
-                return true;
-            }
+        catch (IOException exception) {
+            System.err.println("Errore!");
         }
-
-        return false;
+        catch (InterruptedException exception) {
+            System.err.println("Errore!");
+        }
     }
-
-    public void eliminaTessera(int id) {
-
-        tessereReg.remove(id);
-
-    }
-
 }
