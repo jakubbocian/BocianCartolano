51dc40f28cca39f1ae7f4165044e29be67c61eeb
ZTDavide <dadocarto503@gmail.com>
2022-10-07 12:37:24 +0200

    Richieste 2 e 3
    Richieste 2 e 3 (ancora da testare manca il client)

--- ServerCassonetto/src/ServerCassonetto/ServerCassonetto.java
+++ ServerCassonetto/src/ServerCassonetto/ServerCassonetto.java
@@ -4,13 +4,14 @@ import java.io.*;
 import java.net.*;
 import java.nio.*;
 import java.util.ArrayList;
+import java.util.Date;
 
-public class ServerCassonetto extends Thread{
-    
-    private ArrayList <Tessera> tessere = new ArrayList();
+public class ServerCassonetto extends Thread {
+
+    private ArrayList<Tessera> tessere = new ArrayList();
 
     private DatagramSocket socket;
-    
+
     // costruttore (richiede il numero di porta del servizio)
     public ServerCassonetto(int port) throws SocketException {
         socket = new DatagramSocket(port);
@@ -21,47 +22,133 @@ public class ServerCassonetto extends Thread{
         byte[] buffer = new byte[1024];
         ByteBuffer data;
         DatagramPacket answer, request;
-        
+
         int idTessera;
         System.out.println("Attendo richiesta....");
-        
+
         while (!Thread.interrupted()) {
             try {
                 request = new DatagramPacket(buffer, buffer.length);
-                // attesa ricezione datagram di richiesta (tempo massimo di attesa: 1s)
                 socket.receive(request);
-                // incapsulazione del buffer della richiesta in un byte-buffer della dimensione di 4 valori float
                 data = ByteBuffer.wrap(buffer, 0, 1);
-                
+
                 int richiesta = data.get();
-               
-                
-                if(richiesta == 1){
-                    
-                    System.out.println("Richiesta 1");
+
+                if (richiesta == 1) {
+
+                    System.out.println("Richiesta 1...");
                     data = ByteBuffer.wrap(buffer, 0, 4);
                     data.putInt(creaTessera());
-                    answer = new DatagramPacket( data.array(), 4, request.getAddress(),
-                    request.getPort()); 
+                    answer = new DatagramPacket(data.array(), 4, request.getAddress(),
+                    request.getPort());
                     socket.send(answer);
+
+                } else if (richiesta == 2) {
+
+                    System.out.println("Richiesta 2...");
+
+                    byte[] buffer_t = new byte[4];
+                    ByteBuffer data_t;
+                    DatagramPacket request_t;
+
+                    boolean ricevuta = false;
+                    int id_disattiva = -1;
+
+                    while (!ricevuta) {
+
+                        System.out.println("Attendo tessera da disattivare...");
+
+                        request_t = new DatagramPacket(buffer_t, buffer_t.length);
+                        socket.receive(request_t);
+
+                        data_t = ByteBuffer.wrap(buffer_t, 0, 4);
+
+                        id_disattiva = data_t.getInt();
+
+                        for (Tessera t : tessere) {
+
+                            if (t.getId() == id_disattiva) {
+                                t.setValida(false);
+                                ricevuta = true;
+                            }
+
+                        }
+
+                    }
+
+                } else if (richiesta == 3) {
+
+                    System.out.println("Richiesta 3...");
+
+                    byte[] buffer_t = new byte[1];
+                    ByteBuffer data_t;
+                    DatagramPacket request_t;
+
+                    boolean ricevuta = false;
+                    int id_controlla = -1;
+                    int ris=1;
                     
+                    while (!ricevuta) {
+
+                        System.out.println("Attendo tessera da controllare...");
+
+                        request_t = new DatagramPacket(buffer_t, buffer_t.length);
+                        socket.receive(request_t);
+
+                        data_t = ByteBuffer.wrap(buffer_t, 0, 4);
+
+                        id_controlla = data_t.getInt();
+
+                        for (Tessera t : tessere) {
+
+                            if (t.getId() == id_controlla) {
+
+                                if (!t.isValida() || !controlla_data(t.getU_apertura())) {
+                                    ris = 0;
+                                }
+
+                                ricevuta = true;
+                            }
+
+                        }
+
+                    }
+
+                    data = ByteBuffer.wrap(buffer, 0, 4);
+                    data.putInt(ris);
+                    answer = new DatagramPacket(data.array(), 4, request.getAddress(),
+                    request.getPort());
+                    socket.send(answer);
+
                 }
-                
-                /*
-                // incapsulazione del buffer della risposta in un byte-buffer della dimensione di 1 valore double
-                data = ByteBuffer.wrap(buffer, 0, 8);
-                // inserimento del valore double nel byte-buffer
-                // costruzione del datagram da trasmettere a partire dal contenuto del byte-buffer
-                answer = new DatagramPacket( data.array(), 8, request.getAddress(),
-                request.getPort()); socket.send(answer);
-                socket.send(answer);*/
-            }
-            catch (SocketTimeoutException exception) {
-            }
-            catch (IOException exception) {
+
+            } catch (SocketTimeoutException exception) {
+            } catch (IOException exception) {
             }
         }
         socket.close(); // chiusura del socket
+    }
+
+    public boolean controlla_data(Date d) {
+
+        Date current_date = new Date();
+
+        long time_difference = d.getTime() - current_date.getTime();
+        /*int differenza_ore = ((int)time_difference / (1000*60*60)) % 24;  
+        
+        if(differenza_ore < 72)
+            return false;
+        
+         */
+
+        int differenza_secondi = ((int) time_difference / 1000) % 60;
+
+        if (differenza_secondi < 7) {
+            return false;
+        }
+
+        return true;
+
     }
 
     public static void main(String[] args) {
@@ -73,33 +160,19 @@ public class ServerCassonetto extends Thread{
             c = System.in.read();
             server.interrupt();
             server.join();
-        }
-        catch (IOException exception) {
+        } catch (IOException exception) {
             System.err.println("IO!");
-        }
-        catch (InterruptedException exception) {
+        } catch (InterruptedException exception) {
             System.err.println("IE!");
         }
     }
-    
+
     public int creaTessera() {
 
         Tessera t = new Tessera(this.tessere.size() + 1);
         tessere.add(t);
         return t.getId();
 
-    }
-
-    public boolean checkTessera(int id) {
-
-        for (Tessera t : tessere) {
-
-            if (t.getId() == id) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     public void eliminaTessera(int id) {
